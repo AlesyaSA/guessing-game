e05bc2b746344535e1864e58421ede652367011d
Alesya <saauser77@mail.ru>
2017-01-31 17:23:01 +0300

    guessing-game/feachure:methods added

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) { //this method accepts min and max value of range of number to guess
-
+var a=(min+max);
+alert (Math.round(a / 2)) ;
     }
-
     guess() {//this method returns solution candidate (you make an assumption based on range and previous assumptions)
-
+    return a;
     }
 
     lower() {//this method is called if prev call of `guess()` returned number which is lower than answer
-
+      max=a;
+      return this;
     }
 
     greater() {//this method is called if prev call of `guess()` returned number which is greater than answer
-
+      min=a;
+      return this;
     }
 }
 
