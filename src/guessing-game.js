d8cc78e130e24d85f11f164f14f6798613c1461f
Alesya <saauser77@mail.ru>
2017-01-31 18:52:22 +0300

    guessing-game/feachure:this added in methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,21 +2,31 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) { //this method accepts min and max value of range of number to guess
-var a=(min+max);
-alert (Math.round(a / 2)) ;
+			//var a=(min+max);
+			//alert (Math.round(a / 2)) ;
+			this.min = min;
+			this.max = max;
+			this.a=(Math.round((this.min+this.max)/2));
+
+
+
     }
     guess() {//this method returns solution candidate (you make an assumption based on range and previous assumptions)
-    return a;
+
+
+		return this.a;
     }
 
     lower() {//this method is called if prev call of `guess()` returned number which is lower than answer
-      max=a;
-      return this;
+
+    this.max = a;
+		return this.a;
     }
 
     greater() {//this method is called if prev call of `guess()` returned number which is greater than answer
-      min=a;
-      return this;
+      this.min = a;
+			return this.a;
+
     }
 }
 
