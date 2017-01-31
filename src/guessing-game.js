f485c34c6e005d6dd3d9bf002a0b6e983b0d2509
Alesya <saauser77@mail.ru>
2017-01-31 19:09:55 +0300

    guessing-game/feachure:final work

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -6,25 +6,26 @@ class GuessingGame {
 			//alert (Math.round(a / 2)) ;
 			this.min = min;
 			this.max = max;
-			this.a=(Math.round((this.min+this.max)/2));
+			
 
 
 
     }
     guess() {//this method returns solution candidate (you make an assumption based on range and previous assumptions)
 
-
+this.a=(Math.round((this.min+this.max)/2));
 		return this.a;
     }
 
     lower() {//this method is called if prev call of `guess()` returned number which is lower than answer
 
-    this.max = a;
+    this.max =this.a;
+
 		return this.a;
     }
 
     greater() {//this method is called if prev call of `guess()` returned number which is greater than answer
-      this.min = a;
+      this.min = this.a;
 			return this.a;
 
     }
