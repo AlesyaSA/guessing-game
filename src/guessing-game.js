690fe6c6a988cd36e65571b35a66ccd30f0ca4cd
Alesya <saauser77@mail.ru>
2017-02-04 19:44:36 +0300

    guessing-game/feachure : yey...works

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,19 @@
 class GuessingGame {
-    constructor() {}
+	constructor() {}
 
-    setRange(min, max) { //this method accepts min and max value of range of number to guess
-			this.min = min;
-			this.max = max;
-    }
-    guess() {//this method returns solution candidate (you make an assumption based on range and previous assumptions)
-			this.OurGuess=(Math.round((this.min+this.max)/2));//the middle
-			return this.OurGuess;
-    }
-    lower() {//this method is called if prev call of `guess()` returned number which is lower than answer
-    this.max =this.OurGuess;
-    }
-    greater() {//this method is called if prev call of `guess()` returned number which is greater than answer
-      this.min = this.OurGuess;
-	  }
+	setRange(min, max) { //this method accepts min and max value of range of number to guess
+		this.min = min;
+		this.max = max;
+	}
+	guess() { //this method returns solution candidate (you make an assumption based on range and previous assumptions)
+		this.OurGuess = (Math.round((this.min + this.max) / 2)); //the middle
+		return this.OurGuess;
+	}
+	lower() { //this method is called if prev call of `guess()` returned number which is lower than answer
+		this.max = this.OurGuess;
+	}
+	greater() { //this method is called if prev call of `guess()` returned number which is greater than answer
+		this.min = this.OurGuess;
+	}
 }
-
 module.exports = GuessingGame;
